07f05ebb466872f4e1a32194a4cd3124cde3e9d3
Robert McNabb <robertmcnabb@gmail.com>
2019-03-23 23:06:20 +0100

    limit threading to improve performance

--- mmaster_bias_correction.py
+++ mmaster_bias_correction.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 import os
+os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=4
+os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=4 
+os.environ["MKL_NUM_THREADS"] = "1" # export MKL_NUM_THREADS=6
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1" # export VECLIB_MAXIMUM_THREADS=4
+os.environ["NUMEXPR_NUM_THREADS"] = "1" # export NUMEXPR_NUM_THREADS=6
 import argparse
 import multiprocessing as mp
 from glob import glob
