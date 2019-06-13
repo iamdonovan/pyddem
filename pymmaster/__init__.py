5e6ae46426ccb791607b72035012196bef27a721
Bob McNabb <robertmcnabb@gmail.com>
2019-06-13 16:18:57 +0200

    add __init__.py for pymmaster/

---
+++ pymmaster/__init__.py
@@ -0,0 +1,8 @@
+"""
+pymmaster is a python package to remove jitter-related bias in ASTER DEMs using `MicMac ASTER`_.
+
+.. _MicMac ASTER: https://www.mdpi.com/2072-4292/9/7/704
+"""
+import pymmaster.mmaster_tools
+import pymmaster.fit_tools
+import pymmaster.stack_tools
