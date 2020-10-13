d20c0d0bb1a633acdb4692fbdece73a4a8b78ef3
Bob McNabb <robertmcnabb@gmail.com>
2020-10-13 10:52:51 +0100

    update __init__.py to include new modules.

--- pyddem/__init__.py
+++ pyddem/__init__.py
@@ -2,6 +2,8 @@
 pyddem is a python package to process stacks and time series of DEMs.
 """
 import pyddem.fit_tools
+import pyddem.spstats_tools
 import pyddem.stack_tools
-import pyddem.other_tools
 import pyddem.tdem_tools
+import pyddem.vector_tools
+import pyddem.volint_tools
